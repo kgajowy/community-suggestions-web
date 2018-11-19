2c30e1e4d775e93695527e05570607dc66f84ac9
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-11-19 23:21:11 +0100

    Updates

---
+++ precache-manifest.f790034219aee7bc254e66683d4be8f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb0bc7fe2b3065c8fc6c",
+    "url": "/static/js/main.bb0bc7fe.chunk.js"
+  },
+  {
+    "revision": "dd3f3c7cd7bda38e0f07",
+    "url": "/static/js/1.dd3f3c7c.chunk.js"
+  },
+  {
+    "revision": "39e3f83e29bd700ee8ffc619efa918cc",
+    "url": "/index.html"
+  }
+];
