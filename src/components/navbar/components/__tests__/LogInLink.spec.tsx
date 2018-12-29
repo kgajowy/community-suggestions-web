fb47891dcf99abcc596588b40e6e6aacaf0fdf62
Kamil Gajowy <kamil.gajowy@gmail.com>
2018-12-29 20:03:36 +0100

    chore: add sample test

---
+++ src/components/navbar/components/__tests__/LogInLink.spec.tsx
@@ -0,0 +1,29 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import React from "react";
+import { LinkElement } from "../../LinkElement";
+import { LoginLink } from "../LogInLink";
+
+describe("when onClick is provided", () => {
+  let wrapper: ShallowWrapper;
+  let onClick: jest.Mock;
+
+  beforeEach(() => {
+    onClick = jest.fn();
+    wrapper = shallow(<LoginLink logIn={onClick} />);
+  });
+
+  it("shall render", () => {
+    // expect(wrapper.contains(welcome)).toBe(true);
+    // expect(wrapper.contains(welcome)).toEqual(true);
+    console.log(wrapper.debug());
+    expect(wrapper).toBeTruthy();
+  });
+
+  it("shall call the provided callback", () => {
+    wrapper
+      .find(LinkElement)
+      .props()
+      .onClick({} as any);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
