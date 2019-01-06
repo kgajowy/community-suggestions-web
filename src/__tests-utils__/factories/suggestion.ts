9696c02d7296715b7e0c19c666a86bf4db8ac134
Kamil Gajowy <kamil.gajowy@gmail.com>
2019-01-06 20:34:42 +0100

    chore: add more specs to SupportController - logged/supported states

--- src/__tests-utils__/factories/suggestion.ts
+++ src/__tests-utils__/factories/suggestion.ts
@@ -1,19 +1,11 @@
 import Suggestion from "../../shared/interfaces/suggestion";
+import { emptyPerson } from "./user";
 
 export const emptySuggestion = (): Suggestion => ({
   id: Math.random().toString(),
   title: "Suggestion",
   description: "Suggestion Description",
-  author: {
-    id: Math.random().toString(),
-    email: "john@doe.com",
-    name: "John",
-    lastName: "Doe",
-    profile: "none",
-    picture: {
-      medium: "https://api.adorable.io/avatars/139/test@me.com.png",
-    },
-  },
+  author: emptyPerson(),
   voters: [],
   supporters: [],
 });
